168d8b0459971dbc1ae0b64cc99838375f541a91
Marin Leontenko <marin.leontenko@gmail.com>
2021-10-26 14:39:14 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>To-Do List</h1>
     <ul>
       <li v-for="item in ToDoItems" :key="item.id">
-        <to-do-item :label="item.label" :done="true"></to-do-item>
+        <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
       </li>
     </ul>
   </div>
