1ba88f38a4b569dd9c033385cfafc5e203ae2830
Marin Leontenko <marin.leontenko@gmail.com>
2021-10-27 08:48:18 +0200

    styling

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -1,19 +1,22 @@
 <template>
-    <form @submit.prevent="onSubmit">
-        <label for="new-todo-input">
+  <form @submit.prevent="onSubmit">
+    <h2 class="label-wrapper">
+      <label for="new-todo-input" class="label__lg">
         What needs to be done?
-        </label>
-        <input
-        type="text"
-        id="new-todo-input"
-        name="new-todo"
-        autocomplete="off"
-        v-model="label"
-        />
-        <button type="submit">
-        Add
-        </button>
-    </form>
+      </label>
+    </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+      class="input__lg"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">
+      Add
+    </button>
+  </form>
 </template>
 
 <script>
